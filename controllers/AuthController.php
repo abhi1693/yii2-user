4ea3130226abc46288311d69f8202d2705090949
Abhimanyu Saharan <abhi16_93@yahoo.in>
2015-02-04 01:12:28 +0530

    Initial Commit

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -2,12 +2,15 @@
 
 	namespace abhimanyu\user\controllers;
 
+	use abhimanyu\user\models\LoginForm;
 	use yii\web\Controller;
 
 	class AuthController extends Controller
 	{
 		public function actionLogin()
 		{
-			return $this->render('login');
+			$model = new LoginForm();
+
+			return $this->render('login', ['model' => $model]);
 		}
 	}
---
+++ views/auth/login.php
@@ -0,0 +1,40 @@
+<?php
+	/* @var $model \abhimanyu\user\models\LoginForm */
+?>
+<div class="container" style="text-align: center">
+	<div class="panel panel-default" id="login-form" style="max-width: 300px;margin: 0 auto 20px;text-align: left">
+		<div class="panel-heading"><strong>Please</strong> Sign In!</div>
+		<div class="panel-body">
+			<?php $form = \yii\widgets\ActiveForm::begin([
+				                                             'id'                   => 'login-form',
+				                                             'enableAjaxValidation' => TRUE
+			                                             ]); ?>
+
+			<div class="form-group">
+				<?= $form->field($model, 'username')->textInput(['class' => 'form-control']) ?>
+			</div>
+			<div class="form-group">
+				<?= $form->field($model, 'password')->passwordInput(['class' => 'form-control']) ?>
+			</div>
+			<div class="checkbox">
+				<?= $form->field($model, 'rememberMe')->checkbox() ?>
+			</div>
+			<hr>
+
+			<div class="row">
+				<div class="col-md-4">
+					<?= \yii\helpers\Html::submitButton('Sign In', ['class' => 'btn btn-large btn-primary']) ?>
+				</div>
+
+				<div class="col-md-8 text-right">
+					<small>
+						Forgot your
+						password? <?= \yii\helpers\Html::a('Create a new one!', Yii::$app->urlManager->createUrl('//user/auth/recoverPassword')) ?>
+					</small>
+				</div>
+			</div>
+
+			<?php $form::end(); ?>
+		</div>
+	</div>
+</div>
