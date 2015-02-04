886e2fdce85eae93064bbd2d22380da5debdc300
Abhimanyu Saharan <abhi16_93@yahoo.in>
2015-02-04 12:00:12 +0530

    Added login action

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -3,13 +3,34 @@
 	namespace abhimanyu\user\controllers;
 
 	use abhimanyu\user\models\LoginForm;
+	use Yii;
 	use yii\web\Controller;
+	use yii\web\Response;
+	use yii\widgets\ActiveForm;
 
 	class AuthController extends Controller
 	{
 		public function actionLogin()
 		{
+			// If the user is logged in, redirect to dashboard
+			if (!Yii::$app->user->isGuest)
+				return $this->redirect(Yii::$app->user->returnUrl);
+
+			// todo show register form link if enabled
+
 			$model = new LoginForm();
+
+			if ($model->load(Yii::$app->request->post())) {
+				if (Yii::$app->request->isAjax && $model->load(Yii::$app->request->post())) {
+					Yii::$app->response->format = Response::FORMAT_JSON;
+
+					return ActiveForm::validate($model);
+				}
+
+				if ($model->validate() && $model->login()) {
+					return $this->redirect(Yii::$app->user->returnUrl);
+				}
+			}
 
 			return $this->render('login', ['model' => $model]);
 		}
