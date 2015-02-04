6b23947d72a9c5c11ba6009aa7cb50ed8280ec48
Abhimanyu Saharan <abhi16_93@yahoo.in>
2015-02-04 18:34:30 +0530

    Added profile

--- migrations/m150203_081329_user.php
+++ migrations/m150203_081329_user.php
@@ -1,4 +1,5 @@
 <?php
+	use abhimanyu\user\models\Profile;
 	use abhimanyu\user\models\User;
 	use yii\db\Migration;
 	use yii\db\Schema;
@@ -8,15 +9,23 @@
 		public function up()
 		{
 			$this->createTable(User::tableName(), [
-				'id'          => Schema::TYPE_PK,
-				'username'   => Schema::TYPE_STRING . ' NOT NULL',
-				'email'      => Schema::TYPE_STRING . ' NOT NULL',
-				'password'   => Schema::TYPE_STRING . ' NOT NULL',
-				'super_admin' => Schema::TYPE_INTEGER,
-				'auth_key'    => Schema::TYPE_STRING,
-				'status'     => Schema::TYPE_INTEGER . ' NOT NULL',
-				'created_at' => Schema::TYPE_DATETIME . ' NOT NULL',
-				'updated_at' => Schema::TYPE_DATETIME . ' NOT NULL',
+				'id'            => Schema::TYPE_PK,
+				'username'      => Schema::TYPE_STRING . ' NOT NULL',
+				'email'         => Schema::TYPE_STRING . ' NOT NULL',
+				'password_hash' => Schema::TYPE_STRING . ' NOT NULL',
+				'super_admin'   => Schema::TYPE_INTEGER,
+				'auth_key'      => Schema::TYPE_STRING,
+				'status'        => Schema::TYPE_INTEGER . ' NOT NULL',
+				'created_at'    => Schema::TYPE_DATETIME . ' NOT NULL',
+				'updated_at'    => Schema::TYPE_DATETIME . ' NOT NULL',
+			]);
+
+			$this->createTable(Profile::tableName(), [
+				'id'         => Schema::TYPE_PK,
+				'uid'        => Schema::TYPE_INTEGER . ' NOT NULL',
+				'name_first' => Schema::TYPE_STRING . ' NOT NULL',
+				'name_last'  => Schema::TYPE_STRING,
+				'sex'        => Schema::TYPE_INTEGER,
 			]);
 		}
 
---
+++ models/Profile.php
@@ -0,0 +1,12 @@
+<?php
+
+	namespace abhimanyu\user\models;
+
+	class Profile
+	{
+
+		public static function tableName()
+		{
+			return '{{%user_profile}}';
+		}
+	}
