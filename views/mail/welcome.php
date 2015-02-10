027d701f6a338d377d10349e32cfb22fc7ca4461
Abhimanyu Saharan <abhi16_93@yahoo.in>
2015-02-10 19:02:44 +0530

    fixed confirmation route

--- views/mail/welcome.php
+++ views/mail/welcome.php
@@ -13,11 +13,11 @@
 <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
 	Hello,<br/>
 	Click on the following link to activate your account:<br/>
-	<?= Html::a('Activate Account', Yii::$app->urlManager->createUrl('//user/registration/confirm/') .
-	                                $user->auth_key, ['class' => 'btn btn-lg btn-success']) ?>
+	<?= Html::a('Activate Account', ['//user/registration/confirm/', 'id' => $user->id, 'code' => $user->auth_key],
+	            ['class' => 'btn btn-lg btn-success']) ?>
 </p>
 <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
-	Your account on <?= Yii::$app->name ?> has been successfully created. You can use your email address to log in.
+	Your account on <?= Yii::$app->name ?> has been successfully created. You can use your username address to log in.
 </p>
 <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
 	Email Id: <?= $user->email ?><br>
