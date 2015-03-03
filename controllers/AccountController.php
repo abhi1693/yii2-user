dbbb7f2b8d686771eea7ec26bf2d371c41841aa5
Abhimanyu Saharan <abhi16_93@yahoo.in>
2015-03-03 11:07:57 +0530

    [+] avatar

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -22,7 +22,7 @@
 					'class' => AccessControl::className(),
 					'rules' => [
 						[
-							'actions' => ['profile', 'avatar'],
+							'actions' => ['profile', 'avatar', 'upload-avatar'],
 							'allow'   => TRUE,
 							'roles'   => ['@'],
 						],
@@ -33,7 +33,7 @@
 
 		public function actionProfile()
 		{
-			$profile = Profile::findOne(['uid' => \Yii::$app->user->getId()]);
+			$profile = Profile::findOne(['uid' => Yii::$app->user->getId()]);
 
 			if ($profile->load(Yii::$app->request->post()) && $profile->save()) {
 				Yii::$app->getSession()->setFlash('User Profile successfully updated');
@@ -44,8 +44,28 @@
 
 		public function actionAvatar()
 		{
-			$profile = Profile::findOne(['uid' => \Yii::$app->user->getId()]);
+			$profile = Profile::findOne(['uid' => Yii::$app->user->getId()]);
 
 			return $this->render('avatar', ['profile' => $profile]);
+		}
+
+		public function actionUploadAvatar()
+		{
+			if (isset($_FILES['avatar'])) {
+				$avatar = $_FILES['avatar'];
+				$path   = Yii::$app->basePath . '/../uploads/profile_image/';
+
+				$model         = Profile::findOne(['uid' => Yii::$app->user->getId()]);
+				$model->avatar = 'vendor/abhi1693/yii2-user/uploads/profile_image/' . $avatar['name'];
+
+				if ($model->save())
+					if (move_uploaded_file($avatar['tmp_name'], $path . $avatar['name']))
+						return json_encode((object)NULL);
+					else {
+						return json_encode(['error' => 'An Error Occurred. Please try again!']);
+					}
+			}
+
+			return json_encode(['error' => 'An Error Occurred. Please try again!']);
 		}
 	}
--- views/account/avatar.php
+++ views/account/avatar.php
@@ -9,8 +9,11 @@
 	use kartik\alert\AlertBlock;
 	use kartik\file\FileInput;
 	use yii\helpers\Html;
+	use yii\helpers\Url;
+	use yii\widgets\ActiveForm;
 
 	/** @var $this \yii\web\View */
+	/** @var $profile \abhimanyu\user\models\Profile */
 
 	$this->title = 'Upload Avatar - ' . Yii::$app->name;
 ?>
@@ -30,7 +33,32 @@
 		<div class="panel panel-default">
 			<div class="panel-heading"><?= Html::encode($this->title) ?></div>
 			<div class="panel-body">
-				<?= FileInput::widget() ?>
+				<?= Html::img(Yii::$app->homeUrl . '/../../' . $profile['avatar'], ['class' => 'img-responsive']) ?>
+
+				<hr>
+
+				<?php $form = ActiveForm::begin([
+					                                'enableAjaxValidation' => FALSE,
+					                                'options'              => [
+						                                'enctype' => 'multipart/form-data'
+					                                ]
+				                                ]); ?>
+				<?= $form->field($profile, 'avatar')->widget(
+					FileInput::className(), [
+					'options'       => [
+						'multiple' => FALSE,
+						'accept'   => 'image/*'
+					],
+					'pluginOptions' => [
+						'browseClass'  => 'btn btn-primary btn-block',
+						'browseIcon'   => '<i class="glyphicon glyphicon-camera"></i> ',
+						'browseLabel'  => 'Select Photo',
+						'maxFileCount' => 1,
+						'uploadUrl'    => Url::to(['/user/account/upload-avatar'])
+					],
+				]) ?>
+
+				<?php $form::end(); ?>
 			</div>
 		</div>
 	</div>
