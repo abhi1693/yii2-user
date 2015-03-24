458c05a3d5830f94a170f6c3d767b234314379e4
Abhimanyu Saharan <abhimanyu@teamvulcans.com>
2015-03-24 11:17:08 +0530

    Update OAuth

--- controllers/SettingsController.php
+++ controllers/SettingsController.php
@@ -1,73 +1,80 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Abhimanyu
+ * Date: 12-03-2015
+ * Time: 09:53
+ */
+
+namespace abhimanyu\user\controllers;
+
+use abhimanyu\installer\helpers\Configuration;
+use abhimanyu\installer\helpers\enums\Configuration as Enum;
+use abhimanyu\user\models\SettingsForm;
+use Yii;
+use yii\authclient\clients\GoogleOAuth;
+use yii\authclient\Collection;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+
+class SettingsController extends Controller
+{
+
 	/**
-	 * Created by PhpStorm.
-	 * User: Abhimanyu
-	 * Date: 12-03-2015
-	 * Time: 09:53
+	 * @inheritdoc
 	 */
-
-	namespace abhimanyu\user\controllers;
-
-	use abhimanyu\installer\helpers\Configuration;
-	use abhimanyu\installer\helpers\enums\Configuration as Enum;
-	use abhimanyu\user\models\SettingsForm;
-	use Yii;
-	use yii\authclient\clients\GoogleOAuth;
-	use yii\authclient\Collection;
-	use yii\filters\AccessControl;
-	use yii\web\Controller;
-
-	class SettingsController extends Controller
+	public function behaviors()
 	{
-
-		/**
-		 * @inheritdoc
-		 */
-		public function behaviors()
-		{
-			return [
-				'access' => [
-					'class' => AccessControl::className(),
-					'rules' => [
-						[
-							'allow'   => TRUE,
-							'actions' => ['index'],
-							'roles'   => ['@'],
-						]
+		return [
+			'access' => [
+				'class' => AccessControl::className(),
+				'rules' => [
+					[
+						'allow'   => TRUE,
+						'actions' => ['index'],
+						'roles'   => ['@'],
 					]
-				],
-			];
-		}
+				]
+			],
+		];
+	}
 
-		public function actionIndex()
-		{
-			$model                     = new SettingsForm();
-			$model->canRegister        = Yii::$app->config->get(Enum::USER_REGISTRATION);
-			$model->canRecoverPassword = Yii::$app->config->get(Enum::USER_FORGOT_PASSWORD);
-			$model->google = Yii::$app->config->get(Enum::GOOGLE_AUTH);
-			$model->googleClientId = Yii::$app->config->get(Enum::GOOGLE_CLIENT_ID);
-			$model->googleClientSecret = Yii::$app->config->get(Enum::GOOGLE_CLIENT_SECRET);
+	public function actionIndex()
+	{
+		$model = new SettingsForm();
+		$model->canRegister = Yii::$app->config->get(Enum::USER_REGISTRATION);
+		$model->canRecoverPassword = Yii::$app->config->get(Enum::USER_FORGOT_PASSWORD);
 
-			$config = Configuration::get();
+		$model->googleClientId = Yii::$app->config->get(Enum::GOOGLE_CLIENT_ID);
+		$model->googleClientSecret = Yii::$app->config->get(Enum::GOOGLE_CLIENT_SECRET);
 
-			if ($model->load(Yii::$app->request->post())) {
-				Yii::$app->config->set(Enum::USER_REGISTRATION, $model->canRegister);
-				Yii::$app->config->set(Enum::USER_FORGOT_PASSWORD, $model->canRecoverPassword);
+		$config = Configuration::get();
+
+		if ($model->load(Yii::$app->request->post())) {
+			Yii::$app->config->set(Enum::USER_REGISTRATION, $model->canRegister);
+			Yii::$app->config->set(Enum::USER_FORGOT_PASSWORD, $model->canRecoverPassword);
+			Yii::$app->config->set(Enum::GOOGLE_CLIENT_ID, $model->googleClientId);
+			Yii::$app->config->set(Enum::GOOGLE_CLIENT_SECRET, $model->googleClientSecret);
+
+			if (!empty($model->googleClientId) && !empty($model->googleClientSecret)) {
 				Yii::$app->config->set(Enum::GOOGLE_AUTH, GoogleOAuth::className());
 				Yii::$app->config->set(Enum::GOOGLE_CLIENT_ID, $model->googleClientId);
 				Yii::$app->config->set(Enum::GOOGLE_CLIENT_SECRET, $model->googleClientSecret);
 
-				$config['components']['authClientCollection']['class']                             = Collection::className();
+				$config['components']['authClientCollection']['class'] = Collection::className();
 				$config['components']['authClientCollection']['clients']['google']['class'] = GoogleOAuth::className();
-				$config['components']['authClientCollection']['clients']['google']['clientId']     = $model->googleClientId;
+				$config['components']['authClientCollection']['clients']['google']['clientId'] = $model->googleClientId;
 				$config['components']['authClientCollection']['clients']['google']['clientSecret'] = $model->googleClientSecret;
-
-
-				Configuration::set($config);
-
-				Yii::$app->getSession()->setFlash('success', 'User module settings saved successfully');
+			} else {
+				$config['components']['authClientCollection'] = NULL;
 			}
 
-			return $this->render('index', ['model' => $model]);
+
+			Configuration::set($config);
+
+			Yii::$app->getSession()->setFlash('success', 'User module settings saved successfully');
 		}
+
+		return $this->render('index', ['model' => $model]);
 	}
+}
